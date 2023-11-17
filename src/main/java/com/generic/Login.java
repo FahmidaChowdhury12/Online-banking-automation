0b3fcbb2188fcdac7168dcb45f8fa314f5956d38
fahmidachowdhury12 <fahmidac12@gmail.com>
2023-11-16 20:53:05 -0500

    first save

---
+++ src/main/java/com/generic/Login.java
@@ -0,0 +1,13 @@
+package com.generic;
+
+public class Login {
+
+	int b =20; // name = b ==> global ==> instance
+	static int c= 40; // name = c ==> global ==> static
+	public static void main(String[] args) {
+		
+		int a =10; // v name = a==> local 
+		
+
+}
+}
